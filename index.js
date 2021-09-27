4c2753a3f073690a3c192e9c672791c13d076ce5
Lautaro Carro <lautaroecarro@gmail.com>
2021-09-27 12:55:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch')
 
-const currenciesNames = ["bitcoin", "cardano", "solana", "binancecoin", "matic-network", "hive", "plant-vs-undead-token", "cryptozoon", "smooth-love-potion", "axie-infinity"]
-const currenciesAbreb = ["BTC", "ADA", "SOL", "BNB", "MATIC", "HIVE", "PVU", "ZOON", "SLP", "AXS"]
+const currenciesNames = ["bitcoin", "ethereum", "cardano", "solana", "binancecoin", "matic-network", "hive", "plant-vs-undead-token", "cryptozoon", "smooth-love-potion", "axie-infinity"]
+const currenciesAbreb = ["BTC", "ETH", "ADA", "SOL", "BNB", "MATIC", "HIVE", "PVU", "ZOON", "SLP", "AXS"]
 
 
 const url = `https://api.coingecko.com/api/v3/simple/price?ids=${currenciesNames.join(",")}&vs_currencies=usd&include_last_updated_at=true`
